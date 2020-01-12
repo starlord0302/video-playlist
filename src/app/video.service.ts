45cae216e3e7da0aad9a35b723a6dc1b8cf09796
Károly Kovács <karoly0302@gmail.com>
2020-01-13 00:27:14 +0100

    feat(app): create video service

---
+++ src/app/video.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import { Video } from './video/video';
+
+const API_URL = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  model = '/video';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Accept':  'application/json',
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getUrl() {
+    return `${API_URL}${this.model}`;
+  }
+
+  allVideos() {
+    return this.http.get<Video[]>(this.getUrl(), this.httpOptions);
+  }
+}
