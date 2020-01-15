4fe8252a54bc6f8aa3c91f4bb8426a7b053d1553
Károly Kovács <karoly0302@gmail.com>
2020-01-15 22:57:45 +0100

    feat(video/video-table): move description to an expanded row

--- src/app/video/video-table/video-table.component.ts
+++ src/app/video/video-table/video-table.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 import { Video } from '../video';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-video-table',
   templateUrl: './video-table.component.html',
-  styleUrls: ['./video-table.component.scss']
+  styleUrls: ['./video-table.component.scss'],
+  animations: [
+    trigger('descriptionExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],  
 })
-export class VideoTableComponent implements OnInit {
+export class VideoTableComponent implements OnChanges{
 
-  displayedColumns: string[] = ['id', 'title', 'length', 'description'];
-  @Input() dataSource: Video[];
+  displayedColumns: string[] = ['title', 'length'];
+  @Input() videos: Video[];
+  dataSource = new MatTableDataSource([]);
+  expandedElement: Video | null;
+
+  ngOnChanges() {
+    this.dataSource.data = this.videos as any;
+  }
+
+  convertSecondsToMinutes(length: number): string {
+    const minutes = Math.floor((length / 60)) % 60;
+    const seconds = length % 60;
+    return `${minutes}:${seconds}`;
+  }
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
