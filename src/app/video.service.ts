f64236b4c3fc04aeba94b4076d578589924f757f
Károly Kovács <karoly0302@gmail.com>
2020-01-15 22:18:44 +0100

    feat(video/video-form): update form with http request

--- src/app/video.service.ts
+++ src/app/video.service.ts
@@ -1,24 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-import { environment } from 'src/environments/environment';
-import { Video } from './video/video';
+import { environment } from "src/environments/environment";
+import { Video } from "./video/video";
+import { FormGroup } from "@angular/forms";
 
 const API_URL = environment.apiUrl;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class VideoService {
-
-  model = '/videos';
+  model = "/videos";
   httpOptions = {
     headers: new HttpHeaders({
-      'Accept':  'application/json',
+      Accept: "application/json"
     })
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUrl() {
     return `${API_URL}${this.model}`;
@@ -26,5 +26,15 @@ export class VideoService {
 
   allVideos() {
     return this.http.get<Video[]>(this.getUrl(), this.httpOptions);
+  }
+
+  saveVideo(video) {
+    const postOptions = {
+      headers: this.httpOptions.headers.append(
+        "Content-type",
+        "application/json"
+      )
+    };
+    return this.http.post<Video>(this.getUrl(), video, postOptions);
   }
 }
--- src/app/video/video-form/video-form.component.ts
+++ src/app/video/video-form/video-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { VideoService } from "src/app/video.service";
+import { Video } from "../video";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-video-form",
@@ -14,7 +17,32 @@ export class VideoFormComponent implements OnInit {
     description: [""]
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private videoService: VideoService,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
+
+  calculateSeconds(video: FormGroup): number {
+    return video.value.minutes * 60 + video.value.seconds;
+  }
+
+  createVideo(): Video {
+    return {
+      id: null,
+      title: this.videoForm.value.title,
+      length: this.calculateSeconds(this.videoForm),
+      description: this.videoForm.value.description
+    };
+  }
+
+  saveVideo() {
+    this.videoService.saveVideo(this.createVideo()).subscribe(
+      res => console.log(),
+      err => console.log(err),
+      () => this.router.navigate(["/videos"])
+    );
+  }
 }
